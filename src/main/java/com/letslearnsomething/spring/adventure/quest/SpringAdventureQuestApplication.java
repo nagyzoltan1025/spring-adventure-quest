ed25093f4046a4ca1b123c73bed7d57c5e45ba7c
nagyzoltan1025 <36644322+nagyzoltan1025@users.noreply.github.com>
2024-04-22 22:20:57 +0200

    Generate the base project with initializr

---
+++ src/main/java/com/letslearnsomething/spring/adventure/quest/SpringAdventureQuestApplication.java
@@ -0,0 +1,13 @@
+package com.letslearnsomething.spring.adventure.quest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAdventureQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAdventureQuestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/letslearnsomething/spring/adventure/quest/SpringAdventureQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.letslearnsomething.spring.adventure.quest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAdventureQuestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
